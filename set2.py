33fed4ea1f38b6c3ae788674342b232275eb2be3
unknown <sach.jbp@gmail.com>
2018-10-28 18:32:25 -0400

    Minor changes to part 1 and 2

--- set2.py
+++ set2.py
@@ -49,35 +49,47 @@ bloom_filter = None
 num_words = 0         # number in data stream
 num_words_in_set = 0  # number in Bloom filter's set
 
-a=bitarray()
-for i in range(size):
-    a.append(False)
+bloom_filter=bitarray([0]*size)
+
+import string
+OrdDict=dict()
+
+# Make a dictionary of ascii letters as keys and their binary form as values so as to use them to later to 
+#convert words to binary values
+for char in string.ascii_letters:   
+    OrdDict[char]=format(ord(char), 'b')
 
 for word in bloom_filter_set(): # add the word to the filter by hashing etc.
-        wordB=''.join(format(ord(x), 'b') for x in word)
+        try :
+            wordB=''.join(OrdDict[char] for char in word)
+        except:
+            wordB=''.join(format(ord(x), 'b') for x in word)
         wordB=int(wordB,2)
-
-        a[hash_fns[0](wordB)]=True
-        a[hash_fns[1](wordB)]=True
-        a[hash_fns[2](wordB)]=True
-        a[hash_fns[3](wordB)]=True
-        a[hash_fns[4](wordB)]=True
+        bloom_filter[hash_fns[0](wordB)]=True
+        bloom_filter[hash_fns[1](wordB)]=True
+        bloom_filter[hash_fns[2](wordB)]=True
+        bloom_filter[hash_fns[3](wordB)]=True
+        bloom_filter[hash_fns[4](wordB)]=True
         num_words+=1
 fp=0
 k=0
+    
 for word in data_stream():  # check for membership in the Bloom filter
-        wordB=''.join(format(ord(x), 'b') for x in word)
-        wordB=int(wordB,2)      
+        try :
+            wordB=''.join(OrdDict[char] for char in word)
+        except:
+            wordB=''.join(format(ord(x), 'b') for x in word)
+        wordB=int(wordB,2)     
         num_words_in_set+=1
-        if   (a[hash_fns[0](wordB)]==True and a[hash_fns[1](wordB)]==True and a[hash_fns[2](wordB)]==True and a[hash_fns[3](wordB)]==True and a[hash_fns[4](wordB)]==True):    
+        if   (bloom_filter[hash_fns[0](wordB)]==True and bloom_filter[hash_fns[1](wordB)]==True and bloom_filter[hash_fns[2](wordB)]==True and bloom_filter[hash_fns[3](wordB)]==True and bloom_filter[hash_fns[4](wordB)]==True):    
              k+=1
-        if   a[hash_fns[0](wordB)]==True and a[hash_fns[1](wordB)]==True and a[hash_fns[2](wordB)]==True and a[hash_fns[3](wordB)]==True and a[hash_fns[4](wordB)]==True and not (word in bloom_filter_set()):
+        if   bloom_filter[hash_fns[0](wordB)]==True and bloom_filter[hash_fns[1](wordB)]==True and bloom_filter[hash_fns[2](wordB)]==True and bloom_filter[hash_fns[3](wordB)]==True and bloom_filter[hash_fns[4](wordB)]==True and not (word in bloom_filter_set()):
             fp+=1
         
                          
 print("Total number of False positives",fp)
 print("False positive Percentage",fp/k * 100)
-print('Total number of words in stream = %s'%(num_words,))
+print('Total number of words in proper.txt = %s'%(num_words,))
 print('Total number of words in stream = %s'%(num_words_in_set,))
       
 ################### Part 2 ######################
@@ -91,32 +103,27 @@ def num_trailing_bits(n):
 
     n: integer
     """
-    
     b=bin(n)
-    #print(b)
-    bt=b[-1]
-    #print(bt)
-    tb=0
-    i=1
-    while(not (int(bt) == 1 or bt=='b') ) :
-        tb+=1
-        i+=1
-        bt=b[-i]
-    #print(tb)
+    b1=str(b).rstrip('0')
+    tb=len(b)-len(b1)
     return tb
     
-
 num_distinct = 0
 j=0
 maxTB=[0]*35
+
 for word in data_stream(): # Implement the Flajolet-Martin algorithm
-    
-        wordB=''.join(format(ord(x), 'b') for x in word)
+        try :
+            wordB=''.join(OrdDict[char] for char in word)
+        except:
+            wordB=''.join(format(ord(x), 'b') for x in word)
         wordB=int(wordB,2)
         for i in range(35):
             if maxTB[i] < num_trailing_bits(fm_hash_functions[i](wordB)):
                 maxTB[i] = num_trailing_bits(fm_hash_functions[i](wordB))
         j+=1
+        
+print(maxTB)
 AvgEst=[]*5
 for i in range(0,35,7):
     AvgEst.append(np.average(maxTB[i:i+7]))
